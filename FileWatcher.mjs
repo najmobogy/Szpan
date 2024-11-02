65c756b28839e9aee90f34d3d18362ed13490d08
Bogumil <najmobogi@gmail.com>
2024-11-02 18:45:09 +0100

    lekkie poprawki zadan zrobilem 3

--- FileWatcher.mjs
+++ FileWatcher.mjs
@@ -1,20 +1,26 @@
 import fs from 'fs'
 
+
+
+
+
+
+
 fs.watch("./testfolder", (eventType, filename)=>{
     if(filename){
-            try {
-                fs.appendFileSync('logs.txt', `wykryto zdarzenie ${eventType}, w pliku ${filename} \n`);
-                console.log('Zapisano do pliku!');
+        try {
+        fs.appendFileSync('logs.txt', `wykryto zdarzenie ${eventType}, w pliku ${filename} \n`);
+        console.log('Zapisano do pliku!');
             } catch (error) {
-                console.error('Błąd podczas zapisu do pliku:', error);
-            }
+        console.error('Błąd podczas zapisu do pliku:', error);
+        }
     } else{
         try {
-            fs.appendFileSync('logs.txt', `zdarzenie${eventType} \n`);
+    fs.appendFileSync('logs.txt', `zdarzenie${eventType} \n`);
             console.log('Zapisano do pliku!');
-        } catch (error) {
-            console.error('Błąd podczas zapisu do pliku:', error);
-        }
+    } catch (error) {
+    console.error('Błąd podczas zapisu do pliku:', error);
+    }
     }
 })
 
