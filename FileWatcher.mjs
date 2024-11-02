d565589554e6b15031ec241b014440de84bb7cf7
Bogumil <najmobogi@gmail.com>
2024-11-02 17:28:23 +0100

    DodanieFileWatch

---
+++ FileWatcher.mjs
@@ -0,0 +1,21 @@
+import fs from 'fs'
+
+fs.watch("./testfolder", (eventType, filename)=>{
+    if(filename){
+            try {
+                fs.appendFileSync('logs.txt', `wykryto zdarzenie ${eventType}, w pliku ${filename} \n`);
+                console.log('Zapisano do pliku!');
+            } catch (error) {
+                console.error('Błąd podczas zapisu do pliku:', error);
+            }
+    } else{
+        try {
+            fs.appendFileSync('logs.txt', `zdarzenie${eventType} \n`);
+            console.log('Zapisano do pliku!');
+        } catch (error) {
+            console.error('Błąd podczas zapisu do pliku:', error);
+        }
+    }
+})
+
+console.log("nasluchiwanie zmian")
